92f7b559f7518ac5a3a71ce85431267fe09796ce
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-06-27 12:05:56 -0300

    chore: update dependencies and integrate Tailwind CSS
    - Added Tailwind CSS, PostCSS, and Autoprefixer to package.json.
    - Included Tailwind CSS in the Vite configuration.
    - Created app.css to import Tailwind CSS styles.
    - Updated app.html to link the new app.css stylesheet.
    - Modified the main page (+page.svelte) to use Tailwind CSS classes for styling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let { children } = $props();
+  import "../app.css";
+</script>
+
+<h1 class="font-bold flex text-3xl items-center justify-center">
+  Welcome to SvelteKit
+</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
